b9149c282bdbb4d8602b5a07b5c9ad14e1a1c499
VivekPaswan24 <vivek.paswan0@gmail.com>
2022-12-18 19:06:35 +0530

    getElementByClassName and TagName

---
+++ Task5/question.js
@@ -0,0 +1,10 @@
+let items=document.getElementsByClassName("list-group-item")
+for(let i=0;i<items.length;i++)
+{
+    items[i].style.backgroundColor="#f4f4f4";
+}
+
+let li=document.getElementsByTagName('li')
+for(let i=0;i<li.length;i++){
+    li[i].style.backgroundColor="#f4f4f4";
+}
