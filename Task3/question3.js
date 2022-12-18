a46cb903f7cf4fcf66dbce9126da1b51ce286ba4
VivekPaswan24 <vivek.paswan0@gmail.com>
2022-12-18 18:31:26 +0530

    second commit

---
+++ Task3/question3.js
@@ -0,0 +1,5 @@
+let header=document.getElementById("main-header");
+header.style.borderBottom="solid 3px black"
+let additem=document.getElementsByClassName("title")
+additem[0].innerText="ADD ITEM"
+additem[0].style.fontWeight="bold"
