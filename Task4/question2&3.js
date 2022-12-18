0e56730678cde15693813e7912d04455e21653c3
VivekPaswan24 <vivek.paswan0@gmail.com>
2022-12-18 18:48:25 +0530

    set bgcolor and bold item list

---
+++ Task4/question2&3.js
@@ -0,0 +1,5 @@
+let itemlist=document.getElementsByClassName("list-group-item")
+itemlist[2].style.backgroundColor="green"
+for(let i=0;i<itemlist.length;i++){
+    itemlist[i].style.fontWeight="bold"
+}
